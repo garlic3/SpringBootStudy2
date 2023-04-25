c6c07c58cf528a9eb11cc5bb09d171e9e209a65e
garlic3 <yup_yup@naver.com>
2023-04-25 18:45:32 +0900

    Hello 컨트롤러 테스트

--- helloboot/src/main/java/minspring/helloboot/HelloController.java
+++ helloboot/src/main/java/minspring/helloboot/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello(String name){
         return "Hello " + name;
     }
+
+    // 응답결과 :
+    // status : 200
+    // Content-Type : text/plain;charset=UTF-8
+    // content : Hello spring
 }
