ad66996f33d3931b5ae245963bd3f5cc565bb910
garlic3 <yup_yup@naver.com>
2023-04-25 23:18:53 +0900

    @Value 와 PropertySourcesPlaceHolderConfigurer

--- helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package minspring.config.autoconfig;
 
 import minspring.config.ConditionalMyOnClass;
 import minspring.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -14,12 +15,15 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env){
+    public ServletWebServerFactory servletWebServerFactory(){
 
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
