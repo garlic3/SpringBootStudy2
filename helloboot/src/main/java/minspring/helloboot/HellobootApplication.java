68d806411e571f9f90be5f717b19ffcd35f38748
garlic3 <yup_yup@naver.com>
2023-04-25 18:56:43 +0900

    서블릿 등록

--- helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
@@ -5,12 +5,29 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			// 서블릿 등록
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+					// 응답 생성
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello"); // 서블릿 매핑
+
+		});
 		// 톰캣 서블릿 컨테이너 시작
 		webServer.start();
 
