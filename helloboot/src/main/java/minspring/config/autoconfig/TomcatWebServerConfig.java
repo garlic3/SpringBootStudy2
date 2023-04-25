d037b09d6e38826b015f9a1ce1ac215d4c16f276
garlic3 <yup_yup@naver.com>
2023-04-25 22:00:38 +0900

    @Conditional과  Condition

--- helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,14 +3,23 @@ package minspring.config.autoconfig;
 import minspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
         return  new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
