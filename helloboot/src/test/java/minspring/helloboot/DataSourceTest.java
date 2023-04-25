daa989e7e145878c8c3b9cfe0bbbf9f11b224cc3
garlic3 <yup_yup@naver.com>
2023-04-26 05:58:30 +0900

    JdbcTemplate 과 트랜잭션 매니저 구성

--- helloboot/src/test/java/minspring/helloboot/DataSourceTest.java
+++ helloboot/src/test/java/minspring/helloboot/DataSourceTest.java
@@ -3,22 +3,19 @@ package minspring.helloboot;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HellobootApplication.class)
-@TestPropertySource("classpath:/application.properties")
-public class DataSourceTest {
 
-    @Autowired
-    DataSource dataSource;
+@HellobootTest
+public class DataSourceTest {
+    @Autowired DataSource dataSource;
 
     @Test
     void connect() throws SQLException {
