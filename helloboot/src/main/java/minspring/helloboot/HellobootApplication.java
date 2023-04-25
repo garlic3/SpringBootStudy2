ebf27aacb0a16c68f1851126f05dd4cff95b02fa
garlic3 <yup_yup@naver.com>
2023-04-25 20:16:08 +0900

    빈 오브젝트의 역활과 구분

--- helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
@@ -8,6 +8,10 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 
+		// 사용자 구성정보 (Component Scan)
+		// 자동 구성정보 (Auto Configuration)
+		// TomcatServletWebServletFactory, DispatcherServlet
+
 	}
 
 
