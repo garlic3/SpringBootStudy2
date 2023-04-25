5d7ac215e850b7c2dba227d9f234844b7d571c1e
garlic3 <yup_yup@naver.com>
2023-04-25 19:02:21 +0900

    프론트 컨트롤러로 전환

--- helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/minspring/helloboot/HellobootApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -20,15 +21,20 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			// 서블릿 등록
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
-					String name = req.getParameter("name");
+					if (req.getRequestURI().equals("/hello") && req .getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					// 응답 생성
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						// 응답 생성
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
+					}
+					else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
 			}).addMapping("/hello"); // 서블릿 매핑
 
