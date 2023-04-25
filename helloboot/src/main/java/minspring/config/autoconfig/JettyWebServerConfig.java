3ea4111666fc933feb25054877f502e396fa9f34
garlic3 <yup_yup@naver.com>
2023-04-25 21:53:49 +0900

    스타터와 Jetty 서버 구성 추가

--- helloboot/src/main/java/minspring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/minspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,16 +1,16 @@
 package minspring.config.autoconfig;
 
 import minspring.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-    @Bean("tomcatServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
-        return  new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
