a9895fc7c705fcbc42a1474123e4bf853cb38327
garlic3 <yup_yup@naver.com>
2023-04-25 21:20:34 +0900

    자동 구성 애노테이션 적용

--- helloboot/src/main/java/minspring/config/MyAutoConfiguration.java
+++ helloboot/src/main/java/minspring/config/MyAutoConfiguration.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {
 
 }
--- helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/minspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,13 +1,13 @@
 package minspring.config.autoconfig;
 
+import minspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
-
 
     @Bean
     public ServletWebServerFactory servletWebServerFactory(){
